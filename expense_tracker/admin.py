e816be1600562e43ec4a8d8638e4f5a5fdac96f3
Danu <bharatdanu24@gmail.com>
2025-06-19 19:10:57 +0530

    sup

--- expense_tracker/admin.py
+++ expense_tracker/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from .models import Expense, Budget
-
-admin.site.register(Expense)
-admin.site.register(Budget)
--- expense_tracker/apps.py
+++ expense_tracker/apps.py
@@ -1,29 +1,6 @@
 from django.apps import AppConfig
-from django.conf import settings
-from django.contrib.auth import get_user_model
-import logging
 
 
 class ExpenseTrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'expense_tracker'
-
-    def ready(self):
-        # Avoid running this during migrations or tests
-        import sys
-        if 'runserver' not in sys.argv and 'gunicorn' not in sys.argv:
-            return
-
-        try:
-            User = get_user_model()
-            if not User.objects.filter(username=settings.DJANGO_SUPERUSER_USERNAME).exists():
-                User.objects.create_superuser(
-                    username=settings.DJANGO_SUPERUSER_USERNAME,
-                    email=settings.DJANGO_SUPERUSER_EMAIL,
-                    password=settings.DJANGO_SUPERUSER_PASSWORD
-                )
-                logging.info("Superuser created (from expense_tracker).")
-            else:
-                logging.info("Superuser already exists (from expense_tracker).")
-        except Exception as e:
-            logging.error(f"Error creating superuser in expense_tracker: {e}")

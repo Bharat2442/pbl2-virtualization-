ba0db0f7d59f48987fa3473294e375690e8a21de
Danu <bharatdanu24@gmail.com>
2025-06-19 14:35:26 +0530

    Add whitenoise for static file handling

--- todo_exptrack/settings.py
+++ todo_exptrack/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +135,8 @@ LOGOUT_REDIRECT_URL = '/login/'
 CSRF_TRUSTED_ORIGINS = [
     "https://pbl2-virtualization-production.up.railway.app"
 ]
+
+
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
